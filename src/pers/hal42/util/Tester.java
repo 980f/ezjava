b71b29dc139c468373d65e0e56623ef13ed410b6
Andy Heilveil <Andy Heilveil>
2017-05-04 14:09:52 -0500

    gutted

--- src/pers/hal42/util/Tester.java
+++ src/pers/hal42/util/Tester.java
@@ -1,162 +1,46 @@
-/**
- *
- * To define a class to be testable, define and fill in the following functions
- * for that class:
- *
- *   public static String Usage() {
- *     return "put the usage string here";
- *   }
- *
- *   public static void Test(String[] args) {
- *     // whatever
- *   }
- *
- * ----
- *
- * All of the public functions of the class except for main()
- * can be used by other classes (like gui stuff) to also run tests
- * (and maybe one day, automated testers!)
- *
- * ----
- *
- *
- * no arguments                 - lists options available for this class's main()
- * 'usage'                      - lists ALL testable classes and their parameters
- * 'usage <classname>'          - lists the parameters for that class
- * '<classname> [<parameters>]' - tests the class
- *
- *
- */
-
 package pers.hal42.util;
+
 
 import pers.hal42.logging.ErrorLogStream;
 import pers.hal42.logging.LogLevelEnum;
+import pers.hal42.timer.*;
 
-import java.lang.reflect.Method;
+public class tester implements TimeBomb {
 
-// +++ possibly deal with threading issues?
-// +++ hopefully we would never call this with two separate threads
+  static ErrorLogStream dbg=ErrorLogStream.getForName("tester", LogLevelEnum.VERBOSE);
 
-public class Tester {
 
-  protected static final ErrorLogStream dbg = ErrorLogStream.getForClass(Tester.class);
+  String id="";
+  StopWatch sw;
+  Alarmum alarm;
 
-  public static String getUsage(String className) {
-    return runMethod(className, usageMethodName, usageArgs, null, false);
+  public void onTimeout(){
+    dbg.WARNING(id+ "");
+    alarm=Alarmer.New(alarm.ticks,this);
   }
 
-  public static void doTest(String className, String[] args) {
-    runMethod(className, testMethodName, testArgs, args, true);
+  public String toString(){
+    return id;
   }
 
-  private static final String [] USAGE = {
-    "",
-    Tester.class.getName() + " usage:",
-    "To run this from the commandline (ezjava.jar could be something else):",
-    "   java -cp ezjava.jar pers.hal42.util.Tester <parameters>",
-    "parameters:",
-    "   <none>                        - list options for this class's main()",
-    "   usage <class1> [<class2> ...] - list parameters for specific classes",
-    "   <classname> [<params>]        - test a class",
-    "NOTE: when typing a classname, 'pers.hal42.' is not required; it is assumed."
-  };
 
-  /**
-   * +++ Do more in here later (do switches and turn off console logging, and make it log plain, etc.)
-   */
+  public tester(String id,int ticks) {
+    this.id=id;
+    sw=new StopWatch();
+    alarm= Alarmer.New(ticks,this);
+  }
+
+  /** tester */
   public static void main(String[] args) {
-    int numArgs = args.length;
-//    ErrorLogStream.Console(ErrorLogStream.VERBOSE);
-    if(numArgs == 0) {
-      dbg.logArray(LogLevelEnum.VERBOSE, null, USAGE);
-    } else {
-      if(usageMethodName.equalsIgnoreCase(args[0])) {
-        if(numArgs > 1) {
-          // just do specific classes
-          System.out.println("");
-          for(int i = 1; i < numArgs; i++) {
-            System.out.println(args[i] + ": " + getUsage(makeClassName(args[i])));
-            System.out.println("----");
-          }
-        } else {
-          dbg.logArray(LogLevelEnum.ERROR,null, USAGE);
-        }
-      } else {
-        // test a class
-        doTest(makeClassName(args[0]), shaveNargs(args, 1));
-      }
+    int number=args.length>0? Integer.parseInt(args[0]):3;
+    tester tester[] = new tester[number];
+    for(int i=tester.length;i-->0;){
+      tester[i]=new tester("t"+i,i*1000);
+    }
+    while(Alarmer.alarmCount()>0){
+      Alarmer.dump(dbg,LogLevelEnum.VERBOSE.ordinal());
+      Thread.yield();
     }
   }
 
-  /**
-   * shaves the first n arguments from the front of the array
-   * (basically makes another array with the first n removed)
-   * serves as a utility function that main() classes can use to pass limited args with
-   */
-
-  public static String [] shaveNargs(String[] args, int n) {
-    int newLen = args.length - n;
-    String[] newArgs = new String[newLen];
-    System.arraycopy(args, n, newArgs, 0, newLen);
-    return newArgs;
-  }
-
-  public static String makeClassName(String unknown) {
-    return unknown.startsWith(classPrefix) ? unknown : classPrefix + unknown;
-  }
-
-  // as parameters to class.getMethod()
-  private static final String testArg[] = {};
-  private static final Class testArgs[] = {testArg.getClass()};
-  private static final Class usageArgs[] = {};
-  // one-case strings
-  private static final String classPrefix = "pers.hal42."; //todo: use or share the logging system's logic here
-  private static final String usageMethodName = "Usage";
-  private static final String testMethodName = "Test";
-  // local utility functions
-  private static boolean validClass(Class c) {
-    return validObject(c, "Class");
-  }
-  private static boolean validMethod(Method m, String name) {
-    return validObject(m, "Method " + name);
-  }
-  private static boolean validObject(Object o, String type) {
-    boolean valid = (o != null);
-    if(!valid) {
-      dbg.ERROR(type + " not found!");
-    }
-    return valid;
-  }
-  private static Method getMethod(Class c, String methodName, Class[] parameterTypes) {
-    Method m = null;
-    if(validClass(c)) {
-      try {
-        m = c.getMethod(methodName, parameterTypes);
-      } catch (Exception e) {
-        // not really anything to do
-      }
-    }
-    return m;
-  }
-  private static String runMethod(String className, String methodName, Class[] classargs, String[] args, boolean debugArgsNoReturn) {
-    String s = null;
-    try {
-      Class c = Class.forName(className);
-      Method m = getMethod(c, methodName, classargs);
-      if(validMethod(m, methodName)) {
-        if(debugArgsNoReturn) {
-          Object oargs[] = {args};
-          m.invoke(null, oargs);
-        } else {
-          s = (String) m.invoke(null);
-        }
-      } else {
-        s = className + " or " + className + "." + methodName + "()' not found!";
-      }
-    } catch (Exception e) {
-      dbg.Caught(e);
-    }
-    return s;
-  }
 }
