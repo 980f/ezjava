954ab74885b2d03f72ef42e3b7c219a1384bc243
andyh <andyh.980f@gmail.com>
2017-07-10 17:53:00 -0500

    prep work for engaging database keepalive.

--- src/pers/hal42/timer/StopWatch.java
+++ src/pers/hal42/timer/StopWatch.java
@@ -3,10 +3,10 @@ package pers.hal42.timer;
 import pers.hal42.lang.DateX;
 
 /**
- * Synch's were removed as being gratuitous protection against ridiculous activity.
+ * No sycnhronized's are used as they would be gratuitous protection against ridiculous activity.
  * If a particular instance is CONTROLLED by different threads then behavior is
  * already impossible to make any good sense out of. The only remaining reason to
- * Synch is to ensure that "long" is atomic. That can be done by multiple reads at
+ * synch is to ensure that "long" is atomic. If that is important can do  multiple reads at
  * far less expense than locks.
  * Synch'ing is only needed for non-atomic data that might get written while it is being read.
  *
@@ -17,8 +17,11 @@ public class StopWatch {
   long stopped;
   boolean running;
 
+  /**
+   * @param hitTheFloorRunning is whether to start the timer as part of construction.
+   */
   public StopWatch(boolean hitTheFloorRunning) {
-    Reset();
+    Kill();
     if (hitTheFloorRunning) {
       Start();
     }
@@ -36,19 +39,34 @@ public class StopWatch {
     return started;
   }
 
+  /** @returns elapsed time as seconds. */
   public double seconds() { //can be read while running
     return ((double) millis()) / Ticks.perSecond;
   }
 
+  /** @returns elapsed time as milli-seconds. */
   public long millis() { //can be read while running
     return (running ? DateX.utcNow() : stopped) - started;
   }
 
+  /**
+   * @returns elapsed time in ms, and (re)starts timer.
+   */
+  public long roll() {
+    try {
+      return millis();
+    } finally {
+      Start();
+    }
+  }
+
+  /** starts counting. */
   public void Start() {
     stopped = started = DateX.utcNow();
     running = true;
   }
 
+  /** @returns elapsed time and stops counting */
   public long Stop() {
     if (running) {
       stopped = DateX.utcNow(); // +++ potentially make this Math.max(started, DateX.utcNow());??WHY
@@ -57,7 +75,10 @@ public class StopWatch {
     return millis();
   }
 
-  public void Reset() {
+  /**
+   * forget you may have ever been interested in a time
+   */
+  public void Kill() {
     running = false;
     stopped = started = 0;
   }
