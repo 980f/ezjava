8b42095d848258af5c364c656bdece5d5e5f1ad9
andyh <andy.980f@gmail.com>
2017-07-20 17:40:43 -0500

    database diff seems to be running well.

--- src/pers/hal42/lang/MapX.java
+++ src/pers/hal42/lang/MapX.java
@@ -2,7 +2,10 @@ package pers.hal42.lang;
 
 import pers.hal42.logging.ErrorLogStream;
 
+import java.text.MessageFormat;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.function.Predicate;
 
 /**
  * Created by andyh on 7/19/17.
@@ -13,15 +16,20 @@ public class MapX<K, V> {
 
   private static ErrorLogStream dbg = ErrorLogStream.getForClass(MapX.class);
 
-  public static class Diff<V> {
-    public V thisValue;
-    public V thatValue;
-
-    public Diff(V thiss,V that){
-      thisValue=thiss;
-      thatValue=that;
+  /**
+   * remove diff records where 'thatValue' is null and 'thisValue' does NOT meet criterion @param keep
+   */
+  public static <K, V> int DropNullIfNot(Map<K, Diff<V>> diffs, Predicate<V> keep) {
+    int startingSize = diffs.size();
+    Iterator<K> it = diffs.keySet().iterator();
+    while (it.hasNext()) {
+      K k = it.next();
+      Diff<V> dif = diffs.get(k);
+      if ((dif.thatValue == null) && !keep.test(dif.thisValue)) {  //todo:1 add complementary case wrt who is null
+        it.remove();
+      }
     }
-
+    return startingSize - diffs.size();
   }
 
   @SuppressWarnings("unchecked")
@@ -32,10 +40,10 @@ public class MapX<K, V> {
       thiss.forEach((k, v) -> {
         V probate = that.get(k);
         if (probate != null ? !probate.equals(v):v !=null) {
-            diffs.put(k, new Diff(v,probate));
+          diffs.put(k, new Diff(v,probate));
         }
       });
-      that.forEach((k,v)->{
+      that.forEach((k, v)->{
         if(!thiss.containsKey(k)){
           diffs.put(k, new Diff(null,v));
         }
@@ -46,6 +54,27 @@ public class MapX<K, V> {
       return null;
     }
 
+  }
+
+  public static class Diff<V> {
+    public V thisValue;
+    public V thatValue;
+
+    public Diff(V thiss, V that) {
+      thisValue = thiss;
+      thatValue = that;
+    }
+
+    @Override
+    public String toString() {
+      if (thisValue == null) {
+        return "New:" + thatValue;
+      } else if (thatValue == null) {
+        return "Lost:" + thisValue;
+      } else {
+        return MessageFormat.format("Change:{0}->{1}", thisValue, thatValue);
+      }
+    }
   }
 
 }
