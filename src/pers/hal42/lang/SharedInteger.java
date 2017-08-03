46ae1808b6016ef2cd68fac346284a5c5c1751fa
andyh <andyh.980f@gmail.com>
2017-08-03 14:18:34 -0500

    moving towards generating table crossref entries.

---
+++ src/pers/hal42/lang/SharedInteger.java
@@ -0,0 +1,28 @@
+package pers.hal42.lang;
+
+/**
+ * Created by Andy on 8/3/2017.
+ * <p>
+ * a modifiable integer that can be passed around.
+ * <p>
+ * There are such classes elsewhere but I can't for the life of me remember where they are.
+ */
+public class SharedInteger {
+  int raw;
+
+  public SharedInteger(int raw) {
+    this.raw = raw;
+  }
+
+  public SharedInteger() {
+    this(0);
+  }
+
+  public int next() {
+    return ++raw;
+  }
+
+  public int raw() {
+    return raw;
+  }
+}
