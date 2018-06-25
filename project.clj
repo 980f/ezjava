4cbdeac9bf7a81b14794e4376a04ceb77470b3f6
Matt Hudson <mhudson@fedfis.com>
2018-06-25 16:53:54 -0500

    remove JCL stubs

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
-(defproject ezjava-980f "1.0.0-theriodontia" ;; theme: dinosaurs
+(defproject ezjava-980f "0.1.0-SNAPSHOT"
   :description "EZJava: conveniences for writing crash-proof java programs"
   :url "https://github.com/980f/ezjava"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.jsoup/jsoup "1.10.2"]
-    [mysql/mysql-connector-java "5.1.43"]
-    [org.jetbrains/annotations "15.0"] ]
+  :dependencies [
+    [mysql/mysql-connector-java "5.1.46"]
+    [org.jsoup/jsoup "1.11.3"]
+    [org.jetbrains/annotations "15.0"]
+  ]
   :omit-source true
-  :source-paths ["src"]
   :java-source-paths ["src"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
